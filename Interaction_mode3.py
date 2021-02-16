103fc54cc1141533318b2002d016e2b11593fb89
nooroka <51080708+nooroka@users.noreply.github.com>
2021-02-17 01:07:56 +0300

    Update Interaction_mode3.py

--- Interaction_mode3.py
+++ Interaction_mode3.py
@@ -3,15 +3,12 @@
 import os
 import sys
 import subprocess
-#os.chdir("/home/nooroka/pfam1/")
 listtxt = []
 opch = open("pfamcheck4.txt","r")
 for file1 in opch:
-#for file1 in os.listdir("."):
     file1 = file1.strip()
     listtxt.append(file1) #положила файлы из pfam в список
 os.chdir("/home/nooroka/dna3")
-#os.chdir("/home/npidb/data/pdb_new/dna")
 listtxt2 = [] 
 '''ниже - если файл днкшный, то добавляем в окончательный список'''
 for filenew in listtxt:
@@ -26,12 +23,6 @@ for file6 in sorted(listtxt2):
     process = subprocess.Popen(command)
     code = process.wait()
 
-    #print(file6) #eсли тут print поставить, то один файл печатается
-  #  program = "python"
-  #  arguments = ["Interaction_modemain.py", file6]
-  #  os.execvp(program, (program,) + tuple(arguments))
-  #  print(file6)
- #   print("check")
 
 
 #итог: останавливает проход по файлам именно os.execvp (выяснено по print("check"))
