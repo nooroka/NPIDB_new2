28fb6444e3282abbb9f6213a3ec8970e73e69344
nooroka <51080708+nooroka@users.noreply.github.com>
2021-03-08 03:31:31 +0300

    Update sqlcountcorrone_structs.py

--- without_resolution/sqlcountcorrone_structs.py
+++ without_resolution/sqlcountcorrone_structs.py
@@ -10,12 +10,10 @@ for line in op:
         line[1] = line[1].split()
         line[1] = sorted(line[1])
         key,value = ' '.join(map(str,line[1])),line[0]
-        if value not in data[key]:
-        #if value not in data[key] and str(value) in structsgood:            
+        if value not in data[key]:          
             data[key].append(value)
     if len(line) == 1:
        data[" "].append(value)
-#op2.write(str(data))
 print(data)
 for key in data:
     op3.write(str(key)+"\t"+str(len(data[key]))+"\n")
